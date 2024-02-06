9b4182bb6351a2e13806958d52fbe54a86f6c8c6
Divyansh <srivas.divyansh22@gmail.com>
2024-02-06 11:13:19 +0530

    Prompt issue resolved

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from flask_ngrok import run_with_ngrok
 from io import BytesIO
-from PyPDF2 import PdfFileReader
+from PyPDF2 import PdfReader
 import google.generativeai as palm
 
 app = Flask(__name__)
@@ -16,19 +16,20 @@ def index():
 @app.route('/process_pdf', methods=['POST'])
 def process_pdf():
     pdf_data = request.files['pdf'].read()
-
     if pdf_data:
-        reader = PdfFileReader(BytesIO(pdf_data))
+        reader = PdfReader(BytesIO(pdf_data))
         txt = ""
+        summary = ""
         for i in range(len(reader.pages)):
             page = reader.pages[i]
-            txt += page.extractText()
-
-        pt = 'Generate the summary of the following text in atleast 500 words with report values and give the final diagonasis and recommend health advice that the patient should follow:\n' + txt
-        response = palm.generate_text(prompt=pt)
-
-        if response and response.result:
-            summary = response.result
+            txt = page.extract_text()
+            pt = 'Generate the summary of the following text in atleast 100 words and suggest health precautions that patient should change\n' + txt
+            response = palm.generate_text(prompt=pt)
+            print(f"{response.result}\n\n")
+            if response.result:
+                summary += response.result + "\n\n"
+        # print(txt)
+        if summary != "":
             return jsonify({'summary': summary})
 
     return jsonify({'error': 'Failed to process the PDF'})
